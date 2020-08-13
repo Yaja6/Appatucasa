236ec467e22a15ef989607fbacc8f0d2938fd1dd
Yajaira Cuatis <56648539+Yaja6@users.noreply.github.com>
2020-08-13 01:23:50 -0500

    #1 Modelos y migraciones de tablas

---
+++ database/migrations/2020_08_13_011230_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('date');
+            $table->time('time');
+            $table->integer('quantity');
+            $table->float('finalprice');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
