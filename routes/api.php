159819525862acc8d25213db70b505b69025dbe7
chrispaul20191610 <crispaulpolitec2016@gmail.com>
2020-08-13 13:32:44 -0500

    #2 rutas

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,20 @@
 <?php
 
 use Illuminate\Http\Request;
+//admin
+Route::get('administrators', 'AdministratorController@index');
+Route::get('administrators/{administrator}', 'AdministratorController@show');
+Route::post('administrators', 'AdministratorController@store');
+Route::put('administrators/{administrator}', 'AdministratorController@update');
+Route::delete('administrators/{administrator}', 'AdministratorController@delete');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//categorie
+
+Route::get('categories', 'CategorieController@index');
+Route::get('categories/{categories}', 'CategorieController@show');
+Route::post('categories', 'CategorieController@store');
+Route::put('categories/{categories}', 'CategorieController@update');
+Route::delete('categories/{categories}', 'CategorieController@delete');
 
 
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
