807f39f35c6bc634cc324e52ce181fd099967551
chrispaul20191610 <crispaulpolitec2016@gmail.com>
2020-08-13 14:47:21 -0500

    #2 rutas_controladores

--- routes/api.php
+++ routes/api.php
@@ -35,3 +35,47 @@ Route::get('stores/{store}', 'StoreController@show');
 Route::post('stores', 'StoreController@store');
 Route::put('stores/{store}', 'StoreController@update');
 Route::delete('stores/{store}', 'StoreController@delete');
+
+//client
+Route::get('clients', 'ClientController@index');
+Route::get('clients/{client}', 'ClientController@show');
+Route::post('clients', 'ClientController@store');
+Route::put('clients/{client}', 'ClientController@update');
+Route::delete('clients/{client}', 'ClientController@delete');
+
+//feedback
+Route::get('feedbacks', 'FeedbackController@index');
+Route::get('feedbacks/{feedback}', 'FeedbackController@show');
+Route::post('feedbacks', 'FeedbackController@store');
+Route::put('feedbacks/{feedback}', 'FeedbackController@update');
+Route::delete('feedbacks/{feedback}', 'FeedbackController@delete');
+
+//offer
+
+Route::get('offers', 'OfferController@index');
+Route::get('offers/{offer}', 'OfferController@show');
+Route::post('offers', 'OfferController@store');
+Route::put('offers/{offer}', 'OfferController@update');
+Route::delete('offers/{offer}', 'OfferController@delete');
+
+//order
+Route::get('orders', 'OrderController@index');
+Route::get('orders/{order}', 'OrderController@show');
+Route::post('orders', 'OrderController@store');
+Route::put('orders/{order}', 'OrderController@update');
+Route::delete('orders/{offer}', 'OrderController@delete');
+
+//sale
+Route::get('sales', 'SaleController@index');
+Route::get('sales/{sale}', 'SaleController@show');
+Route::post('sales', 'SaleController@store');
+Route::put('sales/{sale}', 'SaleController@update');
+Route::delete('sales/{sale}', 'SaleController@delete');
+
+//store
+
+Route::get('stores', 'StoreController@index');
+Route::get('stores/{store}', 'StoreController@show');
+Route::post('stores', 'StoreController@store');
+Route::put('stores/{store}', 'StoreController@update');
+Route::delete('stores/{store}', 'StoreController@delete');
