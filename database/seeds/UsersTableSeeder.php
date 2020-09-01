65ec4fdcb659ecc20cd47d8ba59451c219495bcd
Yajaira Cuatis <56648539+Yaja6@users.noreply.github.com>
2020-09-01 12:00:00 -0500

    #4 Autenticación

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        $faker = \Faker\Factory::create();
+        // Crear la misma clave para todos los usuarios
+        // conviene hacerlo antes del for para que el seeder
+        // no se vuelva lento.
+        $password = Hash::make('123123');
+        User::create([
+            'name' => 'Administrador',
+            'lastname' => 'Prueba',
+            'email' => 'admin@prueba.com',
+            'address' => 'quito',
+            'password' => $password,
+        ]);
+        // Generar algunos usuarios para nuestra aplicacion
+        for ($i = 0; $i < 9; $i++) {
+            $user = User::create([
+                'name' => $faker->name,
+                'lastname' => $faker->lastName,
+                'email' => $faker->email,
+                'address' => $faker->address,
+                'password' => $password,
+            ]);
+
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,81 +1,77 @@
 <?php
 
 use Illuminate\Http\Request;
-//admin
-Route::get('administrators', 'AdministratorController@index');
-Route::get('administrators/{administrator}', 'AdministratorController@show');
-Route::post('administrators', 'AdministratorController@store');
-Route::put('administrators/{administrator}', 'AdministratorController@update');
-Route::delete('administrators/{administrator}', 'AdministratorController@delete');
 
-//categorie
-
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
 Route::get('categories', 'CategorieController@index');
-Route::get('categories/{categories}', 'CategorieController@show');
-Route::post('categories', 'CategorieController@store');
-Route::put('categories/{categories}', 'CategorieController@update');
-Route::delete('categories/{categories}', 'CategorieController@delete');
+Route::get('products', 'ProductController@index');
+Route::get('offers', 'OfferController@index');
+
+Route::group(['middleware' => ['jwt.verify']], function () {
+
+//categories
+
+
+    Route::get('categories/{categories}', 'CategorieController@show');
+    Route::post('categories', 'CategorieController@store');
+    Route::put('categories/{categories}', 'CategorieController@update');
+    Route::delete('categories/{categories}', 'CategorieController@delete');
 
 
 //product
-Route::get('products', 'ProductController@index');
-Route::get('products/{product}', 'ProductController@show');
-Route::post('products', 'ProductController@store');
-Route::put('products/{product}', 'ProductController@update');
-Route::delete('products/{product}', 'ProductController@delete');
-//dealer
-Route::get('dealers', 'DealerController@index');
-Route::get('dealers/{dealer}', 'DealerController@show');
-Route::post('dealers', 'DealerController@store');
-Route::put('dealers/{dealer}', 'DealerController@update');
-Route::delete('dealers/{dealer}', 'DealerController@delete');
-//store
-Route::get('stores', 'StoreController@index');
-Route::get('stores/{store}', 'StoreController@show');
-Route::post('stores', 'StoreController@store');
-Route::put('stores/{store}', 'StoreController@update');
-Route::delete('stores/{store}', 'StoreController@delete');
 
-//client
-Route::get('clients', 'ClientController@index');
-Route::get('clients/{client}', 'ClientController@show');
-Route::post('clients', 'ClientController@store');
-Route::put('clients/{client}', 'ClientController@update');
-Route::delete('clients/{client}', 'ClientController@delete');
+    Route::get('products/{product}', 'ProductController@show');
+    Route::post('products', 'ProductController@store');
+    Route::put('products/{product}', 'ProductController@update');
+    Route::delete('products/{product}', 'ProductController@delete');
+//dealer
+    Route::get('dealers', 'DealerController@index');
+    Route::get('dealers/{dealer}', 'DealerController@show');
+    Route::post('dealers', 'DealerController@store');
+    Route::put('dealers/{dealer}', 'DealerController@update');
+    Route::delete('dealers/{dealer}', 'DealerController@delete');
+//store
+    Route::get('stores', 'StoreController@index');
+    Route::get('stores/{store}', 'StoreController@show');
+    Route::post('stores', 'StoreController@store');
+    Route::put('stores/{store}', 'StoreController@update');
+    Route::delete('stores/{store}', 'StoreController@delete');
 
 //feedback
-Route::get('feedbacks', 'FeedbackController@index');
-Route::get('feedbacks/{feedback}', 'FeedbackController@show');
-Route::post('feedbacks', 'FeedbackController@store');
-Route::put('feedbacks/{feedback}', 'FeedbackController@update');
-Route::delete('feedbacks/{feedback}', 'FeedbackController@delete');
+    Route::get('feedbacks', 'FeedbackController@index');
+    Route::get('feedbacks/{feedback}', 'FeedbackController@show');
+    Route::post('feedbacks', 'FeedbackController@store');
+    Route::put('feedbacks/{feedback}', 'FeedbackController@update');
+    Route::delete('feedbacks/{feedback}', 'FeedbackController@delete');
 
 //offer
 
-Route::get('offers', 'OfferController@index');
-Route::get('offers/{offer}', 'OfferController@show');
-Route::post('offers', 'OfferController@store');
-Route::put('offers/{offer}', 'OfferController@update');
-Route::delete('offers/{offer}', 'OfferController@delete');
+
+    Route::get('offers/{offer}', 'OfferController@show');
+    Route::post('offers', 'OfferController@store');
+    Route::put('offers/{offer}', 'OfferController@update');
+    Route::delete('offers/{offer}', 'OfferController@delete');
 
 //order
-Route::get('orders', 'OrderController@index');
-Route::get('orders/{order}', 'OrderController@show');
-Route::post('orders', 'OrderController@store');
-Route::put('orders/{order}', 'OrderController@update');
-Route::delete('orders/{offer}', 'OrderController@delete');
+    Route::get('orders', 'OrderController@index');
+    Route::get('orders/{order}', 'OrderController@show');
+    Route::post('orders', 'OrderController@store');
+    Route::put('orders/{order}', 'OrderController@update');
+    Route::delete('orders/{offer}', 'OrderController@delete');
 
 //sale
-Route::get('sales', 'SaleController@index');
-Route::get('sales/{sale}', 'SaleController@show');
-Route::post('sales', 'SaleController@store');
-Route::put('sales/{sale}', 'SaleController@update');
-Route::delete('sales/{sale}', 'SaleController@delete');
+    Route::get('sales', 'SaleController@index');
+    Route::get('sales/{sale}', 'SaleController@show');
+    Route::post('sales', 'SaleController@store');
+    Route::put('sales/{sale}', 'SaleController@update');
+    Route::delete('sales/{sale}', 'SaleController@delete');
 
 //store
 
-Route::get('stores', 'StoreController@index');
-Route::get('stores/{store}', 'StoreController@show');
-Route::post('stores', 'StoreController@store');
-Route::put('stores/{store}', 'StoreController@update');
-Route::delete('stores/{store}', 'StoreController@delete');
+    Route::get('stores', 'StoreController@index');
+    Route::get('stores/{store}', 'StoreController@show');
+    Route::post('stores', 'StoreController@store');
+    Route::put('stores/{store}', 'StoreController@update');
+    Route::delete('stores/{store}', 'StoreController@delete');
+});
